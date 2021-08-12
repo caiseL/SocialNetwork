5884f171fa78445f0063727a23f11608a9c03783
caiseL <cristophertraperoo@gmail.com>
2021-08-12 13:54:54 -0600

    Added middlewares and routers

--- api/src/app.ts
+++ api/src/app.ts
@@ -1,19 +1,21 @@
 import express from "express";
 import mongoose from "mongoose";
 import morgan from "morgan";
-import { index } from "./controllers/index";
+import { index } from "./services/index";
 import { authenticateToken } from "./middleware/authenticateToken";
 import { postRouter } from "./routers/postRouter";
 import { userRouter } from "./routers/userRouter";
-import { checkEnv } from "./validators/checkEnv";
+import { checkEnv } from "./config/checkEnv";
+import { cloudinaryConfig } from "./config/cloudinary";
 
 require("dotenv").config();
 checkEnv();
+cloudinaryConfig();
 
 const app = express();
 const PORT = 3000;
 
-mongoose.connect(process.env.ATLAS_URI as string, {
+mongoose.connect(process.env.DATABASE_URI as string, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
     useFindAndModify: false,
@@ -24,8 +26,8 @@ app.use(express.json());
 app.use(morgan("dev")); //combined?
 
 app.get("/", authenticateToken, index);
-app.use("/posts", authenticateToken, postRouter);
 app.use("/users", userRouter);
+app.use("/posts", authenticateToken, postRouter);
 
 app.listen(PORT, () => {
     console.log("Listening at " + PORT);
