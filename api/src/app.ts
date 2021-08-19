1c2b4310d5eeb36fcedec775dbe776a596d204c0
caiseL <cristophertraperoo@gmail.com>
2021-08-19 17:47:17 -0600

    Added friend feature

--- api/src/app.ts
+++ api/src/app.ts
@@ -1,12 +1,14 @@
 import express from "express";
 import mongoose from "mongoose";
 import morgan from "morgan";
+import multer from "multer";
 import { index } from "./index";
 import { authenticateToken } from "./middleware/authenticateToken";
 import { postRouter } from "./routers/postRouter";
 import { userRouter } from "./routers/userRouter";
 import { checkEnv } from "./config/checkEnv";
 import { cloudinaryConfig } from "./config/cloudinary";
+import { friendRouter } from "./routers/friendRouter";
 
 require("dotenv").config();
 checkEnv();
@@ -27,6 +29,7 @@ app.use(morgan("dev")); //combined?
 
 app.get("/", authenticateToken, index);
 app.use("/users", userRouter);
+app.use("/friends", authenticateToken, friendRouter);
 app.use("/posts", authenticateToken, postRouter);
 
 app.listen(PORT, () => {
